a7c02cb81a336e0a0faedde0d51a0c3fe2a66083
taeseonYoo <jkl0642@hansung.ac.kr>
2025-10-29 19:03:08 +0900

    refactor(Lotto): 로또에서 규칙을 검증하도록 리팩토링
    - 중복되지 않은 숫자로 구성되었는지 검증
    - 로또가 숫자 6개로 구성되었는지 검증
    - 로또가 1~45범위의 정수로 구성되었는지 검증

--- src/main/java/lotto/RandomNumberGenerator.java
+++ src/main/java/lotto/RandomNumberGenerator.java
@@ -6,7 +6,7 @@ import lotto.model.NumberGenerator;
 
 public class RandomNumberGenerator implements NumberGenerator {
     @Override
-    public List<Integer> generate(int min, int max,int count) {
-        return Randoms.pickUniqueNumbersInRange(min, max, count);
+    public List<Integer> generate(int min, int max, int count) {
+        return Randoms.pickUniqueNumbersInRange(min, max, count).stream().sorted().toList();
     }
 }
--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -4,21 +4,39 @@ import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
+    private static final int LOTTO_MIN_NUMBER = 1;
+    private static final int LOTTO_MAX_NUMBER = 45;
+    private static final int LOTTO_COUNT = 6;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        Collections.sort(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
+        verifyNumberSize(numbers);
+        verifyNumberRange(numbers);
+        verifyNumberDuplication(numbers);
+    }
+
+    private void verifyNumberDuplication(List<Integer> numbers) {
+        if (numbers.stream().distinct().count() != LOTTO_COUNT) {
+            throw new IllegalArgumentException("로또 번호는 중복되지 않은 6개로 구성되어야 합니다.");
+        }
+    }
+    private void verifyNumberSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void verifyNumberRange(List<Integer> numbers) {
+        if (!numbers.stream().allMatch(num -> num >= LOTTO_MIN_NUMBER && num <= LOTTO_MAX_NUMBER)) {
+            throw new IllegalArgumentException("로또 번호는 1~45사이의 숫자만 입력할 수 있습니다.");
+        }
+    }
+
     public List<Integer> getNumbers() {
         return Collections.unmodifiableList(numbers);
     }
