8638f165b9caa7166043a1671e35ac55998110dd
taeseonYoo <jkl0642@hansung.ac.kr>
2025-10-30 21:52:53 +0900

    test(Parser): Parser 단위 테스트 작성
    - 당첨 번호 parsing 단위 테스트
    - 구매 금액 parsing 단위 테스트
    - 보너스 숫자 parsing 단위 테스트

---
+++ src/test/java/lotto/utils/ParserTest.java
@@ -0,0 +1,78 @@
+package lotto.utils;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ParserTest {
+
+    @Test
+    @DisplayName("당첨 번호가 분리되어 리스트로 반환된다.")
+    void parsingWinningNumbers_success() {
+        //given
+        String input = "1,2,3,4,5,6";
+        //when
+        List<Integer> numbers = Parser.parsingWinningNumbers(input);
+        //then
+        Assertions.assertThat(numbers).containsExactly(1, 2, 3, 4, 5, 6);
+    }
+
+    @Test
+    @DisplayName("당첨 번호에 int 범위 이상의 값이 입력되면 오류가 발생한다.")
+    void parsingWinningNumbers_fail() {
+        //given
+        String input = "1,2,3,4,5,2147483648";
+        //when & then
+        Assertions.assertThatThrownBy(
+                        () -> Parser.parsingWinningNumbers(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("보너스 숫자가 정수형으로 반환된다.")
+    void parsingBonusNumber_success() {
+        //given
+        String input = "45";
+        //when
+        int bonusNumber = Parser.parsingBonusNumber(input);
+        //then
+        Assertions.assertThat(bonusNumber).isEqualTo(45);
+
+    }
+
+    @Test
+    @DisplayName("보너스 숫자에 int 범위 이상의 값이 입력되면 오류가 발생한다.")
+    void parsingBonusNumber_fail() {
+        //given
+        String input = String.valueOf((long) Integer.MAX_VALUE + 1);
+        //when & then
+        Assertions.assertThatThrownBy(
+                        () -> Parser.parsingBonusNumber(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("구매 금액이 정수형으로 반환된다.")
+    void parsingAmount_success() {
+        //given
+        String input = "5000";
+        //when
+        int amount = Parser.parsingAmount(input);
+        //then
+        Assertions.assertThat(amount).isEqualTo(5000);
+    }
+
+    @Test
+    @DisplayName("구매 금액에 int 범위 이상의 값이 입력되면 오류가 발생한다.")
+    void parsingAmount_fail() {
+        //given
+        String input = String.valueOf((long) Integer.MAX_VALUE + 1);
+        //when & then
+        Assertions.assertThatThrownBy(
+                        () -> Parser.parsingAmount(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
