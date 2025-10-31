a4784efc99261c1c32ac186b8ed333e2f856e3b2
taeseonYoo <jkl0642@hansung.ac.kr>
2025-10-31 21:35:00 +0900

    test(WinningNumbersTest): 당첨 번호 도메인 테스트
    - 구매한 로또와 당첨 번호가 일치하는 개수를 반환하는 로직 테스트
    - 구매한 로또외 보너스 번호가 일치하는 지 검사하는 로직 테스트

--- src/test/java/lotto/model/WinningNumbersTest.java
+++ src/test/java/lotto/model/WinningNumbersTest.java
@@ -2,12 +2,25 @@ package lotto.model;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
 import java.util.List;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class WinningNumbersTest {
+    WinningNumbers winningNumbers;
+
+    @BeforeEach
+    void setUp() {
+        Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        BonusNumber bonusNumber = new BonusNumber(7);
+        winningNumbers = new WinningNumbers(winningLotto, bonusNumber);
+    }
+
     @Test
     @DisplayName("당첨 번호에 보너스 번호가 포함되면 에러가 발생한다.")
     void createWinningNumbers_fail() {
@@ -29,5 +42,46 @@ class WinningNumbersTest {
         BonusNumber bonusNumber = new BonusNumber(7);
         //when & then
         assertDoesNotThrow(() -> new WinningNumbers(winningLotto, bonusNumber));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "1,2,3,4,5,6^6",
+            "1,2,3,4,5,8^5",
+            "1,2,3,4,8,9^4",
+            "1,2,3,8,9,10^3",
+            "8,9,10,11,12,13^0",
+    }, delimiter = '^')
+    @DisplayName("구매한 로또와 당첨 번호가 일치하는 개수를 정확히 반환하는 지 검증한다.")
+    void countMatchingNumbers(String inputNumbers, int answer) {
+        //given
+        List<Integer> numbers = Arrays.stream(inputNumbers.split(",")).map(Integer::parseInt).toList();
+        Lotto purchasedLotto = new Lotto(numbers);
+        //when
+        int match = winningNumbers.countMatchingNumbers(purchasedLotto);
+        //then
+        Assertions.assertThat(match).isEqualTo(answer);
+    }
+
+    @Test
+    @DisplayName("보너스 번호와 일치하지 않으면 false를 반환한다.")
+    void isMatchBonus_false() {
+        //given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        //when
+        boolean matchBonus = winningNumbers.isMatchBonus(lotto);
+        //then
+        Assertions.assertThat(matchBonus).isFalse();
+    }
+
+    @Test
+    @DisplayName("보너스 번호와 일치하면 true를 반환한다.")
+    void isMatchBonus_true() {
+        //given
+        Lotto lotto = new Lotto(List.of(7, 8, 9, 10, 11, 12));
+        //when
+        boolean matchBonus = winningNumbers.isMatchBonus(lotto);
+        //then
+        Assertions.assertThat(matchBonus).isTrue();
     }
 }
